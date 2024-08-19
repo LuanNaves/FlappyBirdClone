861cccde9d08a254b12e8093c1a4f6c0572ad35d
Luan Felipe <luandhx@gmail.com>
2024-08-19 15:03:06 -0300

    Fixing score bugs

--- scripts/global/global.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-
-var SCORE : int = 0
--- scripts/main.gd
+++ scripts/main.gd
@@ -6,25 +6,26 @@ extends Node2D
 @onready var menu = $Menu
 @onready var hud = $HUD
 
-var score: 
+var score = 0: 
 	set(new_score):
 		score = new_score
 		hud.update_score(score)
 
 func _ready():
-	#pipe_spawner.connect("obstacle_created", self, "_on_obstacle_created")
-	pass
+	pipe_spawner.obstacle_created.connect(_on_obstacle_created)
 
 func _on_obstacle_created(obs):
-	obs.connect("score", self, "player_score")
+	obs.score.connect(player_score)
 
 func player_score():
 	self.score += 1
 	
 func new_game():
-	pipe_spawner.start()	
+	score = 0
+	pipe_spawner.start()
 	
 func _on_player_died():
+	hud.visible = false
 	game_over()
 
 func game_over():
@@ -32,7 +33,8 @@ func game_over():
 	floor.stop()
 	background.stop()
 	get_tree().call_group("obstacles", "set_physics_process", false)
-	menu.init_game_over_menu(Global.SCORE)
+	menu.init_game_over_menu(score)
 
 func _on_menu_start_game():
 	new_game()
+
